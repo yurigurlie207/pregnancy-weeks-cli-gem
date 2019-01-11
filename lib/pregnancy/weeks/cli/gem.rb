ae27145f9b4d2fb1937196e722ca78a143ef3bf8
Adrienne Yao <adrienne.yao@icloud.com>
2019-01-11 20:28:12 +0000

    first commit

---
+++ lib/pregnancy/weeks/cli/gem.rb
@@ -0,0 +1,12 @@
+require "pregnancy/weeks/cli/gem/version"
+
+module Pregnancy
+  module Weeks
+    module Cli
+      module Gem
+        class Error < StandardError; end
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/pregnancy/weeks/cli/gem/version.rb
@@ -0,0 +1,9 @@
+module Pregnancy
+  module Weeks
+    module Cli
+      module Gem
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
