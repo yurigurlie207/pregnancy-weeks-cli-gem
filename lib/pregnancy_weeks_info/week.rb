7e936fa91a8c5e23ca27b5bbb4e1224ba2c4bcc1
Adrienne Yao <adrienne.yao@icloud.com>
2019-01-12 20:48:46 +0000

    add new method to week class

--- lib/pregnancy_weeks_info/week.rb
+++ lib/pregnancy_weeks_info/week.rb
@@ -1,2 +1,16 @@
 class PregnancyWeeksInfo::Week
+
+  attr_accessor :weight, :size, :weeks_left
+  @@all = []
+
+  def self.new_from_index_page()
+    self.new()
+  end
+
+  def initialize(weight=nil, size=nil, weeks_left=nil)
+    @weight = weight
+    @size = size
+    @weeks_left = weeks_left
+  end
+  
 end
