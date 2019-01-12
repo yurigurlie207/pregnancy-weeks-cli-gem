a7223a56b09772fd78adb4328b8e0c60f6f72e55
Adrienne Yao <adrienne.yao@icloud.com>
2019-01-12 18:00:25 +0000

    create cli greeting for gem

--- lib/pregnancy_weeks_info/cli.rb
+++ lib/pregnancy_weeks_info/cli.rb
@@ -1,3 +1,16 @@
 class PregnancyWeeksInfo::CLI
 
+  #this method will create the week objects and greets the user
+  def call
+    #create new instances of weeks from pregnancy website
+    puts "Hello, and Welcome to Discovering all the miraculous events during the 42 Weeks of Pregnancy!"
+    start
+  end
+
+  #the start method
+  def start
+    
+  end
+
+
 end
