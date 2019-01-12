98a3b9635df0a8de2752626c4c70a75153176cb8
Adrienne Yao <adrienne.yao@icloud.com>
2019-01-12 17:42:23 +0000

    update environment requirements

---
+++ config/environment.rb
@@ -0,0 +1,8 @@
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+require_relative '../lib/pregnancy_weeks_info/scraper'
+require_relative '../lib/pregnancy_weeks_info/week'
+require_relative '../lib/pregnancy_weeks_info/cli'
+require_relative '../lib/pregnancy_weeks_info/version'
