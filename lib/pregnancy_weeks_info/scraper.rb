0040630a0fa959e55c1b5965f2351b073f86332b
Adrienne Yao <adrienne.yao@icloud.com>
2019-01-15 22:53:40 +0000

    update get subpage

--- lib/pregnancy_weeks_info/scraper.rb
+++ lib/pregnancy_weeks_info/scraper.rb
@@ -5,12 +5,22 @@ class PregnancyWeeksInfo::Scraper
     doc = Nokogiri::HTML(open("https://www.whattoexpect.com/pregnancy/week-by-week/"))
     links = []
 
+    link = doc.css(".sublanding-modules__module__promos__promo__texts__title")[0]['href']
+    binding.pry
 
     42.times do |i|
-      links << doc.css(".sublanding-modules__module__promos__promo__texts__title")[i]['href']
+      link = doc.css(".sublanding-modules__module__promos__promo__texts__title")[i]['href']
+      binding.pry
+      links << link
+      # if link.include? "weeks-1-and-2" then
+      #   links << "https://www.whattoexpect.com" + link
+      #   links << "https://www.whattoexpect.com" + link
+      # elsif link.include? "week-" then
+      #   links << "https://www.whattoexpect.com" + link
+      # end
     end
-    print links
-    binding.pry
+
+    # binding.pry
     #
     # subpages = doc.css(".sublanding-modules__module__promos__promo__texts__title")[href].each do |subpage|
     #   subpage = "https://www.whattoexpect.com/" + subpage
