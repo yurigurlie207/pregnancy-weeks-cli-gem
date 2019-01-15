f30607017d06abbc42383bb73b152862de35b741
Adrienne Yao <adrienne.yao@icloud.com>
2019-01-15 22:36:52 +0000

    update scraper to scrape subpages WIP

--- lib/pregnancy_weeks_info/scraper.rb
+++ lib/pregnancy_weeks_info/scraper.rb
@@ -1,22 +1,39 @@
 class PregnancyWeeksInfo::Scraper
 
-  def get_page
-    Nokogiri::HTML(open("https://www.whattoexpect.com/pregnancy/week-by-week/weeks-1-and-2.aspx"))
+  #
+  def get_sub_pages
+    doc = Nokogiri::HTML(open("https://www.whattoexpect.com/pregnancy/week-by-week/"))
+    links = []
+
+
+    42.times do |i|
+      links << doc.css(".sublanding-modules__module__promos__promo__texts__title")[i]['href']
+    end
+    print links
+    binding.pry
+    #
+    # subpages = doc.css(".sublanding-modules__module__promos__promo__texts__title")[href].each do |subpage|
+    #   subpage = "https://www.whattoexpect.com/" + subpage
+    #   binding.pry
+    # end
+
+  end
+
+
+  def get_page(subpage)
+      Nokogiri::HTML(open(subpage))
   end
 
   def scrape_weeks_index
-    self.get_page.css(".intro")
+    get_sub_pages.each do |subpage|
+      self.get_page(subpage).css(".intro")
+    end
   end
 
   def make_weeks
-    wk = scrape_weeks_index
-
-    # scrape_weeks_index.each do |wk|
+      scrape_weeks_index.each do |wk|
       PregnancyWeeksInfo::Week.new_from_sub_pages(wk)
-    # end
-
-    binding.pry
-
+      end
   end
 
 end
