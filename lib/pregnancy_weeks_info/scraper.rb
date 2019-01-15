ead1811654e5c7b2b93d1ff166997554898ba63c
Adrienne Yao <adrienne.yao@icloud.com>
2019-01-15 23:00:47 +0000

    update and finish get_sub_pages, returns an array of links to subpages

--- lib/pregnancy_weeks_info/scraper.rb
+++ lib/pregnancy_weeks_info/scraper.rb
@@ -1,32 +1,20 @@
 class PregnancyWeeksInfo::Scraper
 
-  #
   def get_sub_pages
     doc = Nokogiri::HTML(open("https://www.whattoexpect.com/pregnancy/week-by-week/"))
     links = []
 
-    link = doc.css(".sublanding-modules__module__promos__promo__texts__title")[0]['href']
-    binding.pry
-
-    42.times do |i|
+    43.times do |i|
       link = doc.css(".sublanding-modules__module__promos__promo__texts__title")[i]['href']
-      binding.pry
-      links << link
-      # if link.include? "weeks-1-and-2" then
-      #   links << "https://www.whattoexpect.com" + link
-      #   links << "https://www.whattoexpect.com" + link
-      # elsif link.include? "week-" then
-      #   links << "https://www.whattoexpect.com" + link
-      # end
+      if link.include? "weeks-1-and-2" then
+        links << "https://www.whattoexpect.com" + link
+        links << "https://www.whattoexpect.com" + link
+      elsif link.include? "week-" then
+        links << "https://www.whattoexpect.com" + link
+      end
     end
 
-    # binding.pry
-    #
-    # subpages = doc.css(".sublanding-modules__module__promos__promo__texts__title")[href].each do |subpage|
-    #   subpage = "https://www.whattoexpect.com/" + subpage
-    #   binding.pry
-    # end
-
+    links
   end
 
 
